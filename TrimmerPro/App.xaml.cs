76ed704985553932f1596511b837a4ecfd05991c
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-10 22:58:33 +0330

    feat: prepare application class with prism design

---
+++ TrimmerPro/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using TrimmerPro.Views;
+using Window = System.Windows.Window;
+
+namespace TrimmerPro;
+
+public partial class App
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        
+    }
+
+    protected override Window CreateShell() => Container.Resolve<MainWindow>();
+}
