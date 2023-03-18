0a1e8aeb130c0c39f5eea8ae2538e10db4b44620
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-18 17:17:18 +0330

    feat: manage DI interface, component registration

--- TrimmerPro/App.xaml.cs
+++ TrimmerPro/App.xaml.cs
@@ -1,16 +1,42 @@
-﻿using System.Windows;
-using Prism.Ioc;
-using TrimmerPro.Views;
-using Window = System.Windows.Window;
-
-namespace TrimmerPro;
+﻿namespace TrimmerPro;
 
 public partial class App
 {
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        GlobalData.Init();
+        if (GlobalData.Config.Skin != SkinType.Default)
+        {
+            UpdateSkin(GlobalData.Config.Skin);
+        }
+
+        Container.Resolve<IRegionManager>().RequestNavigate("ContentRegion", "MainShortener");
+    }
+
     protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
-        
+        containerRegistry.RegisterForNavigation<MainShortenerView>();
+        containerRegistry.RegisterForNavigation<MainContentView>();
+        containerRegistry.RegisterForNavigation<LeftMainContentView>();
+        containerRegistry.RegisterForNavigation<AboutView>();
+        containerRegistry.RegisterForNavigation<SettingView>();
+        containerRegistry.RegisterForNavigation<UpdateView>();
     }
 
     protected override Window CreateShell() => Container.Resolve<MainWindow>();
+
+    public void UpdateSkin(SkinType skin)
+    {
+        Resources.MergedDictionaries.Add(new ResourceDictionary
+        {
+            Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin}.xaml")
+        });
+
+        Resources.MergedDictionaries.Add(new ResourceDictionary
+        {
+            Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+        });
+    }
 }
